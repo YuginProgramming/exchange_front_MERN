74fcc0d663df23873f5658203df7cbe69a5596e0
YuginProgramming <dudaryev@gmail.com>
2022-12-26 19:12:34 +0200

    26.12.22 axios

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from '../../axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const { data } = await axios.get('/posts');
+    return data;
+});
 
 const initialState = {
     posts: {
@@ -16,3 +22,5 @@ const postsSlice = createSlice({
     initialState,
     reducer: {},
 });
+
+export const postsReducer = postsSlice.reducer;
