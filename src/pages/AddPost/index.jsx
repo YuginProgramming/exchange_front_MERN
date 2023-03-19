3418b513177df5116cbcab83c78d4ee02475a9e5
YuginProgramming <dudaryev@gmail.com>
2023-03-19 19:32:12 +0200

    backup_19_03_23

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -7,19 +7,29 @@ import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
-import styles from './AddPost.module.scss';
 import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
+import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
-  const imageUrl = '';
   const isAuth = useSelector(selectIsAuth);
   const [value, setValue] = React.useState('');
   const [title, setTitle] = React.useState('');
   const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
   const inputFileRef = React.useRef(null);
 
   const handleChangeFile = async (event) => {
-    console.log(event.target.files);
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData)
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert('Failed to upload the file!')
+    }
   };
   
   const onClickRemoveImage = () => {};
@@ -54,12 +64,12 @@ export const AddPost = () => {
       </Button>
       <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Delete
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Delete
+          </Button>
+          <img className={styles.image} src={`http://localhost:3333${imageUrl}`} alt="Uploaded" />
+        </>
       )}
       <br />
       <br />
